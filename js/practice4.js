b87dfb4e3653a1052348eb675c8e626eb11848ec
ise0305 <ise03054@gmailcom>
2023-07-14 15:48:54 +0900

    자바스크립트 실습4

---
+++ js/practice4.js
@@ -0,0 +1,8 @@
+let title = document.querySelector('#title');
+title.style.color = 'red';
+
+let box = document.querySelector('.box');
+box.style.color = 'blue';
+box.style.width = '200px';
+box.style.height = '300px';
+box.style.border = 'solid black';
