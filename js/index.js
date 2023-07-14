4787eb035a7e972496401829babba996a8f418da
ise0305 <ise03054@gmailcom>
2023-07-14 16:09:37 +0900

    반복문 문제

--- js/index.js
+++ js/index.js
@@ -212,3 +212,16 @@ function changeColor(){
     title.style.color = colors[index];
 }
 title.addEventListener('click', changeColor);
+
+let data = [
+    {회원번호:0, 아이디:'red', 성별:'남자', 나이:21},
+    {회원번호:1, 아이디:'yello', 성별:'남자', 나이:17},
+    {회원번호:2, 아이디:'orange', 성별:'여자', 나이:16},
+    {회원번호:3, 아이디:'green', 성별:'여자', 나이:55}
+]
+
+for(let i=0; i<data.length; i++){
+    if(data[i].나이 >= 20){
+        console.log(data[i].회원번호);
+    }
+}
